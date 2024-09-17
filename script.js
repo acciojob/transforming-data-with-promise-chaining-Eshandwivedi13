5274043ca46bd3d43f3e07dd5147a82c593c4be0
Eshan Dwivedi <79198616+Eshandwivedi13@users.noreply.github.com>
2024-09-17 16:42:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-//your JS code here. If required.
+const inputNum = document.getElementById('ip');
+const inputBtn = document.getElementById('btn');
+const outputDiv = document.getElementById('output');
+
+inputBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const number = parseFloat(inputNum.value); // Convert input to a number
+
+    // Start the promise chain
+    new Promise((res) => {
+        setTimeout(() => {
+            res(number);
+        }, 2000);
+    })
+    .then((data) => {
+        outputDiv.innerText = `Result: ${data}`;
+        return new Promise((res) => {
+            setTimeout(() => {
+                res(data);
+            }, 2000);
+        });
+    })
+    .then((data) => {
+        outputDiv.innerText = `Result: ${data}`;
+        return new Promise((res) => {
+            setTimeout(() => {
+                res(data * 2); // Multiply by 2
+            }, 1000);
+        });
+    })
+    .then((data) => {
+        outputDiv.innerText = `Result: ${data}`;
+        return new Promise((res) => {
+            setTimeout(() => {
+                res(data - 3); // Subtract 3
+            }, 1000);
+        });
+    })
+    .then((data) => {
+        outputDiv.innerText = `Result: ${data}`;
+        return new Promise((res) => {
+            setTimeout(() => {
+                res(data / 2); // Divide by 2
+            }, 1000);
+        });
+    })
+    .then((data) => {
+        outputDiv.innerText = `Result: ${data}`;
+        return new Promise((res) => {
+            setTimeout(() => {
+                res(data + 10); // Add 10
+            }, 1000);
+        });
+    })
+    .then((data) => {
+        outputDiv.innerText = `Final Result: ${data}`; // Final result
+    })
+    .catch((err) => {
+        outputDiv.innerText = `Error: ${err}`;
+    });
+});
